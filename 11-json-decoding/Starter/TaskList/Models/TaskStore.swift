a01305beb8cc9ce3f3f71b2f386d03cc75430ba5
Vici Shaweddy <vshaweddy@gmail.com>
2020-12-10 20:06:59 -0500

    saving on device

--- 11-json-decoding/Starter/TaskList/Models/TaskStore.swift
+++ 11-json-decoding/Starter/TaskList/Models/TaskStore.swift
@@ -41,11 +41,15 @@ class TaskStore: ObservableObject {
     }
     
     private func loadJSONPrioritizedTasks() {
-        guard let tasksJSONURL = Bundle.main.url(forResource: "PrioritizedTasks",
-                                                 withExtension: "json") else {
-            return
-            
-        }
+        print(Bundle.main.bundleURL)
+        print(FileManager.documentsDirectoryURL)
+        
+        let temporaryDirectoryURL = FileManager.default.temporaryDirectory
+        print(temporaryDirectoryURL)
+        
+        let tasksJSONURL = URL(fileURLWithPath: "PrioritizedTasks", relativeTo: FileManager.documentsDirectoryURL).appendingPathExtension("json")
+        
+        print((try? FileManager.default.contentsOfDirectory(atPath: FileManager.documentsDirectoryURL.path)) ?? [])
         
         let decoder = JSONDecoder()
         
